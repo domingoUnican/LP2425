79d251ebbe322ad7ceb595c46f24efec5fa548f1
Rubén Martínez Amodia <ruben.martinezam@alumnos.unican.es>
2025-04-13 22:21:39 +0200

    Implementar Scanner como Lexer según el libro y modificar representación usando el patrón Visitor

--- Teoria_y_Ejercicios/Lox/Representacion.py
+++ Teoria_y_Ejercicios/Lox/Representacion.py
@@ -1,388 +1,311 @@
-from abc import abstractmethod
-from dataclasses import dataclass
+# Representacion.py
+# Este código es una implementación en Python del intérprete Lox,
+# basada en el libro "Crafting Interpreters" de Robert Nystrom:
+# https://craftinginterpreters.com/
+__author__ = "Rubén Martínez Amodia"
+__version__ = "2025"
 
 from Lexer import Token
-from typing import List, Literal, Tuple, Optional
+
+from abc import ABC, abstractmethod
 
 
-@dataclass
-class Primary:
+class ExprVisitor(ABC):
+    @abstractmethod
+    def visit_assign_expr(self, expr: "Assign"): pass
 
     @abstractmethod
-    def tostring(self):
-        raise NotImplementedError
-
-
-@dataclass
-class NUMBER(Primary):
-    tok: Token
-
-    def tostring(self):
-        s = " " + str(self.tok.tipo) + "\n"
-        s += "   " + self.tok.value
-        return s
-
-
-@dataclass
-class STRING(Primary):
-    tok: Token
-
-    def tostring(self):
-        s = " " + str(self.tok.tipo) + "\n"
-        s += "   " + self.tok.value
-        return s
-
-
-@dataclass
-class IDENTIFIER(Primary):
-    tok: Token
-
-    def tostring(self):
-        s = " " + str(self.tok.tipo) + "\n"
-        s += "   " + self.tok.value
-        return s
-
-
-@dataclass
-class Expression:
+    def visit_binary_expr(self, expr: "Binary"): pass
 
     @abstractmethod
-    def tostring(self):
-        raise NotImplementedError
-
-
-@dataclass
-class Arguments:
-    expr: List[Expression]
-
-    def tostring(self):
-        s = self.expr[0]
-        if len(self.expr) >= 2:
-            for expr in self.expr[1:]:
-                s += ", "
-                s += expr
-        return s
-
-
-@dataclass
-class Parameters:
-    names: List[IDENTIFIER]
-
-    def tostring(self):
-        s = self.names[0]
-        if len(self.names) >= 2:
-            for name in self.names[1:]:
-                s += ", "
-                s += name
-        return s
-
-
-@dataclass
-class Function:
-    name: IDENTIFIER
-    params: Parameters
-    body: "Block"
-
-    def tostring(self):
-        s = self.name
-        s += "("
-        s += self.params.tostring()
-        s += ")"
-        s += self.body.tostring()
-        return s
-
-
-@dataclass
-class Call(Primary):
-    base: Primary
-    args: Optional[Arguments]
-    properties: List[Tuple[IDENTIFIER, Optional[Arguments]]]
-
-    def tostring(self):
-        s = self.base.tostring()
-
-        for prop, args in self.properties:
-            s += "."
-            s += str(prop)
-            if args:
-                s += "("
-                s += args.tostring()
-                s += ")"
-
-        if self.args is not None:
-            s += "("
-            s += self.args.tostring()
-            s += ")"
-
-        return s
-
-
-@dataclass
-class Unary:
-    op: Literal["!", "="]
-    attr: "Unary" | Call
-
-    def tostring(self):
-        s = self.op
-        s += self.attr.tostring()
-        return s
-
-
-@dataclass
-class Factor:
-    unary: Unary
-    other_unary: List[Tuple[Literal["/", "*"], Unary]]
-
-    def tostring(self):
-        s = self.unary.tostring()
-        if self.other_unary:
-            for unary in self.other_unary:
-                s += " " + unary[0] + " "
-                s += unary[1].tostring()
-        return s
-
-
-@dataclass
-class Term:
-    factor: Factor
-    other_factor: List[Tuple[Literal["-", "+"], Factor]]
-
-    def tostring(self):
-        s = self.factor.tostring()
-        if self.other_factor:
-            for factor in self.other_factor:
-                s += " " + factor[0] + " "
-                s += factor[1].tostring()
-        return s
-
-
-@dataclass
-class Comparison:
-    term: Term
-    other_term: List[Tuple[Literal[">", ">=", "<", "<="], Term]]
-
-    def tostring(self):
-        s = self.term.tostring()
-        if self.other_term:
-            for term in self.other_term:
-                s += " " + term[0] + " "
-                s += term[1].tostring()
-        return s
-
-
-@dataclass
-class Equality:
-    comp: Comparison
-    other_comp: List[Tuple[Literal["!=", "=="], Comparison]]
-
-    def tostring(self):
-        s = self.comp.tostring()
-        if self.other_comp:
-            for comp in self.other_comp:
-                s += " " + comp[0] + " "
-                s += comp[1].tostring()
-        return s
-
-
-@dataclass
-class LogicAnd:
-    equality: List[Equality]
-
-    def tostring(self):
-        s = self.equality[0].tostring()
-        if len(self.equality) >= 2:
-            for equality in self.equality[1:]:
-                s += " and "
-                s += equality.tostring()
-        return s
-
-
-@dataclass
-class LogicOr:
-    logic_and: List[LogicAnd]
-
-    def tostring(self):
-        s = self.logic_and[0].tostring()
-        if len(self.logic_and) >= 2:
-            for logic_and in self.logic_and[1:]:
-                s += " or "
-                s += logic_and.tostring()
-        return s
-
-
-@dataclass
-class Assignment(Expression):
-    call: Call
-    name: str
-    assign: "Assignment"
-    logic_or: LogicOr
-
-    def tostring(self):
-        s = ""
-        if self.logic_or:
-            s = self.logic_or.tostring()
-        else:
-            if self.call:
-                s += self.call.tostring() + "."
-            s += self.name
-            s += " = "
-            s += self.assign.tostring()
-        return s
-
-
-@dataclass
-class Declaration:
+    def visit_call_expr(self, expr: "Call"): pass
 
     @abstractmethod
-    def tostring(self):
-        raise NotImplementedError
-
-
-@dataclass
-class ClassDecl(Declaration):
-    name: str
-    father: str
-    methods: List[Function]
-
-    def tostring(self):
-        s = "class"
-        s += " "
-        s += self.name
-        if self.father:
-            s += " < " + self.father
-        return s
-
-
-@dataclass
-class FunDecl(Declaration):
-    fun: Function
-
-    def tostring(self):
-        s = "fun"
-        s += self.fun.tostring()
-        return s
-
-
-@dataclass
-class VarDecl(Declaration):
-    name: str
-    expr: Expression
-
-    def tostring(self):
-        s = "var"
-        s += " "
-        s += self.name
-        if self.expr:
-            s += " = " + self.expr.tostring()
-        return s
-
-
-@dataclass
-class Statement(Declaration):
+    def visit_grouping_expr(self, expr: "Grouping"): pass
 
     @abstractmethod
-    def tostring(self):
-        raise NotImplementedError
+    def visit_literal_expr(self, expr: "Literal"): pass
+
+    @abstractmethod
+    def visit_logical_expr(self, expr: "Logical"): pass
+
+    @abstractmethod
+    def visit_unary_expr(self, expr: "Unary"): pass
+
+    @abstractmethod
+    def visit_variable_expr(self, expr: "Variable"): pass
 
 
-@dataclass
-class ExprStmt(Statement):
-    expr: Expression
+class Expr(ABC):
+    @abstractmethod
+    def accept(self, visitor: ExprVisitor):
+        pass
 
-    def tostring(self):
-        s = self.expr.tostring()
-        s += ";"
-        return s
+    @abstractmethod
+    def tostring(self, n):
+        pass
 
+class Assign(Expr):
+    def __init__(self, name: Token, value: Expr):
+        self.name = name
+        self.value = value
 
-@dataclass
-class ForStmt(Statement):
-    start: VarDecl | ExprStmt | Literal[";"]
-    med: Expression
-    end: Expression
-    stmt: Statement
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_assign_expr(self)
 
-    def tostring(self):
-        s = "for"
-        s += "("
-        s += self.start.tostring()
-        if self.med:
-            s += self.med.tostring()
-        if self.end:
-            s += self.end.tostring()
-        s += ")"
-        s += self.stmt.tostring()
-        return s
+    def tostring(self, n):
+        output = " " * n + f"assign {self.name.lexeme}\n"
+        output += self.value.tostring(n + 2)
+        return output
 
 
-@dataclass
-class IfStmt(Statement):
-    cond: Expression
-    then_stmt: Statement
-    else_stmt: Statement
+class Binary(Expr):
+    def __init__(self, left: Expr, operator: Token, right: Expr):
+        self.left = left
+        self.operator = operator
+        self.right = right
 
-    def tostring(self):
-        s = "if"
-        s += "("
-        s += self.cond.tostring()
-        s += ")"
-        s += self.then_stmt.tostring()
-        if self.else_stmt:
-            s += "else" + self.else_stmt.tostring()
-        return s
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_binary_expr(self)
 
-
-@dataclass
-class PrintStmt(Statement):
-    expr: Expression
-
-    def tostring(self):
-        s = "print"
-        s += " "
-        s += self.expr.tostring()
-        s += ";"
-        return s
+    def tostring(self, n):
+        output = " " * n + f"binary {self.operator.lexeme}\n"
+        output += self.left.tostring(n + 2) + "\n"
+        output += self.right.tostring(n + 2)
+        return output
 
 
-@dataclass
-class ReturnStmt(Statement):
-    expr: Expression
+class Call(Expr):
+    def __init__(self, callee: Expr, paren: Token, arguments: list[Expr]):
+        self.callee = callee
+        self.paren = paren
+        self.arguments = arguments
 
-    def tostring(self):
-        s = "return"
-        if self.expr:
-            s += self.expr.tostring()
-        s += ";"
-        return s
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_call_expr(self)
 
-
-@dataclass
-class WhileStmt(Statement):
-    cond: Expression
-    stmt: Statement
-
-    def tostring(self):
-        s = "while"
-        s += "("
-        s += self.cond.tostring()
-        s += ")"
-        s += self.stmt.tostring()
-        return s
+    def tostring(self, n):
+        output = " " * n + "call\n"
+        output += self.callee.tostring(n + 2) + "\n"
+        for arg in self.arguments:
+            output += arg.tostring(n + 2) + "\n"
+        return output.strip()
 
 
-@dataclass
-class Block(Statement):
-    decl: List[Declaration]
+class Grouping(Expr):
+    def __init__(self, expression: Expr):
+        self.expression = expression
 
-    def tostring(self):
-        s = "{"
-        if self.decl:
-            for decl in self.decl:
-                s += decl.tostring()
-        s += "}"
-        return s
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_grouping_expr(self)
+
+    def tostring(self, n):
+        output = " " * n + "grouping\n"
+        output += self.expression.tostring(n + 2)
+        return output
 
 
-@dataclass
-class Program:
-    declarations: List[Declaration]
+class Literal(Expr):
+    def __init__(self, value: object):
+        self.value = value
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_literal_expr(self)
+
+    def tostring(self, n):
+        return " " * n + f"literal {self.value}"
+
+
+class Logical(Expr):
+    def __init__(self, left: Expr, operator: Token, right: Expr):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_logical_expr(self)
+
+    def tostring(self, n):
+        output = " " * n + f"logical {self.operator.lexeme}\n"
+        output += self.left.tostring(n + 2) + "\n"
+        output += self.right.tostring(n + 2)
+        return output
+
+
+class Unary(Expr):
+    def __init__(self, operator: Token, right: Expr):
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_unary_expr(self)
+
+    def tostring(self, n):
+        output = " " * n + f"unary {self.operator.lexeme}\n"
+        output += self.right.tostring(n + 2)
+        return output
+
+
+class Variable(Expr):
+    def __init__(self, name: Token):
+        self.name = name
+
+    def accept(self, visitor: ExprVisitor):
+        return visitor.visit_variable_expr(self)
+
+    def tostring(self, n):
+        return " " * n + f"variable {self.name.lexeme}"
+
+
+class StmtVisitor(ABC):
+    @abstractmethod
+    def visit_block_stmt(self, stmt: "Block"): pass
+
+    @abstractmethod
+    def visit_expression_stmt(self, stmt: "Expression"): pass
+
+    @abstractmethod
+    def visit_function_stmt(self, stmt: "Function"): pass
+
+    @abstractmethod
+    def visit_if_stmt(self, stmt: "If"): pass
+
+    @abstractmethod
+    def visit_print_stmt(self, stmt: "Print"): pass
+
+    @abstractmethod
+    def visit_return_stmt(self, stmt: "Return"): pass
+
+    @abstractmethod
+    def visit_var_stmt(self, stmt: "Var"): pass
+
+    @abstractmethod
+    def visit_while_stmt(self, stmt: "While"): pass
+
+
+class Stmt(ABC):
+    @abstractmethod
+    def accept(self, visitor: StmtVisitor):
+        pass
+
+    @abstractmethod
+    def tostring(self, n):
+        pass
+
+
+class Block(Stmt):
+    def __init__(self, statements: list[Stmt]):
+        self.statements = statements
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_block_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + "block\n"
+        for stmt in self.statements:
+            output += stmt.tostring(n + 2) + "\n"
+        return output.strip()
+
+
+class Expression(Stmt):
+    def __init__(self, expression: Expr):
+        self.expression = expression
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_expression_stmt(self)
+
+    def tostring(self, n):
+        return " " * n + "expression\n" + self.expression.tostring(n + 2)
+
+
+class Function(Stmt):
+    def __init__(self, name: Token, params: list[Token], body: list[Stmt]):
+        self.name = name
+        self.params = params
+        self.body = body
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_function_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + f"function {self.name.lexeme}(\n"
+        for param in self.params:
+            output += " " * (n + 2) + param.lexeme + "\n"
+        output += " " * n + ")\n"
+        for body_stmt in self.body:
+            output += body_stmt.tostring(n + 2) + "\n"
+        return output
+
+
+class If(Stmt):
+    def __init__(self, condition: Expr, then_branch: Stmt, else_branch: Stmt | None):
+        self.condition = condition
+        self.then_branch = then_branch
+        self.else_branch = else_branch
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_if_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + "if\n"
+        output += self.condition.tostring(n + 2) + "\n"
+        output += self.then_branch.tostring(n + 2) + "\n"
+        if self.else_branch:
+            output += self.else_branch.tostring(n + 2) + "\n"
+        return output.strip()
+
+
+class Print(Stmt):
+    def __init__(self, expression: Expr):
+        self.expression = expression
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_print_stmt(self)
+
+    def tostring(self, n):
+        return " " * n + "print\n" + self.expression.tostring(n + 2)
+
+
+class Return(Stmt):
+    def __init__(self, keyword: Token, value: Expr | None):
+        self.keyword = keyword
+        self.value = value
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_return_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + "return\n"
+        if self.value:
+            output += self.value.tostring(n + 2) + "\n"
+        return output
+
+
+class Var(Stmt):
+    def __init__(self, name: Token, initializer: Expr | None):
+        self.name = name
+        self.initializer = initializer
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_var_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + f"var {self.name.lexeme}\n"
+        if self.initializer:
+            output += " " * (n + 2) + "= \n" + self.initializer.tostring(n + 2)
+        return output
+
+
+class While(Stmt):
+    def __init__(self, condition: Expr, body: Stmt):
+        self.condition = condition
+        self.body = body
+
+    def accept(self, visitor: StmtVisitor):
+        return visitor.visit_while_stmt(self)
+
+    def tostring(self, n):
+        output = " " * n + "while\n"
+        output += self.condition.tostring(n + 2) + "\n"
+        output += self.body.tostring(n + 2) + "\n"
+        return output.strip()
---
+++ Teoria_y_Ejercicios/Lox/Scanner.py
@@ -0,0 +1,229 @@
+# Scanner.py
+# Este código es una implementación en Python del intérprete Lox,
+# basada en el libro "Crafting Interpreters" de Robert Nystrom:
+# https://craftinginterpreters.com/
+__author__ = "Rubén Martínez Amodia"
+__version__ = "2025"
+
+class TokenType:
+    # Single-character tokens.
+    LEFT_PAREN = 'LEFT_PAREN'
+    RIGHT_PAREN = 'RIGHT_PAREN'
+    LEFT_BRACE = 'LEFT_BRACE'
+    RIGHT_BRACE = 'RIGHT_BRACE'
+    COMMA = 'COMMA'
+    DOT = 'DOT'
+    MINUS = 'MINUS'
+    PLUS = 'PLUS'
+    SEMICOLON = 'SEMICOLON'
+    SLASH = 'SLASH'
+    STAR = 'STAR'
+
+    # One or two character tokens.
+    BANG = 'BANG'
+    BANG_EQUAL = 'BANG_EQUAL'
+    EQUAL = 'EQUAL'
+    EQUAL_EQUAL = 'EQUAL_EQUAL'
+    GREATER = 'GREATER'
+    GREATER_EQUAL = 'GREATER_EQUAL'
+    LESS = 'LESS'
+    LESS_EQUAL = 'LESS_EQUAL'
+
+    # Literals.
+    IDENTIFIER = 'IDENTIFIER'
+    STRING = 'STRING'
+    NUMBER = 'NUMBER'
+
+    # Keywords.
+    AND = 'AND'
+    CLASS = 'CLASS'
+    ELSE = 'ELSE'
+    FALSE = 'FALSE'
+    FUN = 'FUN'
+    FOR = 'FOR'
+    IF = 'IF'
+    NIL = 'NIL'
+    OR = 'OR'
+    PRINT = 'PRINT'
+    RETURN = 'RETURN'
+    SUPER = 'SUPER'
+    THIS = 'THIS'
+    TRUE = 'TRUE'
+    VAR = 'VAR'
+    WHILE = 'WHILE'
+
+    EOF = 'EOF'
+
+
+class Token:
+    def __init__(self, type: TokenType, lexeme: str, literal: object, line: int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def tostring(self):
+        return "Token(" + str(self.type) + ", " + str(self.lexeme) + ", " + str(self.literal) + ")"
+
+
+class Scanner:
+    keywords = {
+        "and": TokenType.AND,
+        "class": TokenType.CLASS,
+        "else": TokenType.ELSE,
+        "false": TokenType.FALSE,
+        "for": TokenType.FOR,
+        "fun": TokenType.FUN,
+        "if": TokenType.IF,
+        "nil": TokenType.NIL,
+        "or": TokenType.OR,
+        "print": TokenType.PRINT,
+        "return": TokenType.RETURN,
+        "super": TokenType.SUPER,
+        "this": TokenType.THIS,
+        "true": TokenType.TRUE,
+        "var": TokenType.VAR,
+        "while": TokenType.WHILE,
+    }
+
+    def __init__(self, source):
+        self.source = source
+        self.tokens = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+    def scan_tokens(self):
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def scan_token(self):
+        c = self.advance()
+
+        if c == '(':
+            self.add_token(TokenType.LEFT_PAREN)
+        elif c == ')':
+            self.add_token(TokenType.RIGHT_PAREN)
+        elif c == '{':
+            self.add_token(TokenType.LEFT_BRACE)
+        elif c == '}':
+            self.add_token(TokenType.RIGHT_BRACE)
+        elif c == ',':
+            self.add_token(TokenType.COMMA)
+        elif c == '.':
+            self.add_token(TokenType.DOT)
+        elif c == '-':
+            self.add_token(TokenType.MINUS)
+        elif c == '+':
+            self.add_token(TokenType.PLUS)
+        elif c == ';':
+            self.add_token(TokenType.SEMICOLON)
+        elif c == '*':
+            self.add_token(TokenType.STAR)
+        elif c == '!':
+            self.add_token(TokenType.BANG_EQUAL if self.match('=') else TokenType.BANG)
+        elif c == '=':
+            self.add_token(TokenType.EQUAL_EQUAL if self.match('=') else TokenType.EQUAL)
+        elif c == '<':
+            self.add_token(TokenType.LESS_EQUAL if self.match('=') else TokenType.LESS)
+        elif c == '>':
+            self.add_token(TokenType.GREATER_EQUAL if self.match('=') else TokenType.GREATER)
+        elif c == '/':
+            if self.match('/'):
+                while self.peek() != '\n' and not self.is_at_end():
+                    self.advance()
+            else:
+                self.add_token(TokenType.SLASH)
+        elif c in (' ', '\r', '\t'):
+            pass  # Ignore whitespace
+        elif c == '\n':
+            self.line += 1
+        elif c == '"':
+            self.string()
+        else:
+            if self.is_digit(c):
+                self.number()
+            elif self.is_alpha(c):
+                self.identifier()
+            else:
+                self.error("Unexpected character.")
+
+    def identifier(self):
+        while self.is_alpha_numeric(self.peek()):
+            self.advance()
+
+        text = self.source[self.start:self.current]
+        token_type = self.keywords.get(text, TokenType.IDENTIFIER)
+        self.add_token(token_type)
+
+    def number(self):
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == '.' and self.is_digit(self.peek_next()):
+            self.advance()
+
+            while self.is_digit(self.peek()):
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start:self.current]))
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == '\n':
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            self.error("Unterminated string.")
+            return
+
+        self.advance()
+        value = self.source[self.start + 1:self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def match(self, expected):
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != expected:
+            return False
+        self.current += 1
+        return True
+
+    def peek(self):
+        if self.is_at_end():
+            return '\0'
+        return self.source[self.current]
+
+    def peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.current + 1]
+
+    def is_alpha(self, c):
+        return c.isalpha() or c == '_'
+
+    def is_alpha_numeric(self, c):
+        return self.is_alpha(c) or self.is_digit(c)
+
+    def is_digit(self, c):
+        return c.isdigit()
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+    def advance(self):
+        c = self.source[self.current]
+        self.current += 1
+        return c
+
+    def add_token(self, type, literal=None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
+
+    def error(self, message):
+        print(f"[line {self.line}] Error: {message}")

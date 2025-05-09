3f6c6badc491a070cee8646b172824c5e24be1bd
Hugo Martínez <hugombdc@gmail.com>
2025-05-09 12:18:59 +0200

    Representacion.py fin

--- Teoria_y_Ejercicios/Lox/Representacion.py
+++ Teoria_y_Ejercicios/Lox/Representacion.py
@@ -54,18 +54,21 @@ class Function:
 
 @dataclass
 class Number(Primary):
-    class Number(Primary):
-        def tostring(self, n):
-            output = " " * n + self.value.tipo + "\n"
-            output += " " * (n + 2) + self.value.value + "\n"
-            return output
+    tok: Token
+
+    def tostring(self, n):
+        output = " " * n + str(self.tok.tipo) + "\n"
+        output += " " * (n + 2) + str(self.tok.value)
+        return output
 
 
 @dataclass
 class String(Primary):
+    tok: Token
+
     def tostring(self, n):
-        output  = " " * n     + self.value.tipo + "\n"
-        output += " " * (n+2) + self.value.value + "\n"
+        output = " " * n + str(self.tok.tipo) + "\n"
+        output += " " * (n + 2) + str(self.tok.value)
         return output
 
 @dataclass
@@ -79,10 +82,11 @@ class Unary(Expression):
     atr: Optional["Unary"] = None
 
     def tostring(self, n):
-        output = " " * (n + 2) + "op: " + self.op + "\n"
+        output = " " * (n + 1) + "op:"+ self.op + "\n"
         for element in self.atr:
             output += element.tostring(n + 2) + "\n"
         return output
+
 
 
 @dataclass
@@ -346,7 +350,7 @@ class Arguments(Expression):
 
     def tostring(self, n):
         output = " " * n + "Argument\n"
-        output += self.name + "\n"
+        output += self.name + ", "
         if self.parameters:
             output += self.parameters.tostring(n + 2)
         return output
@@ -354,99 +358,239 @@ class Arguments(Expression):
 
 @dataclass
 class Factor:
-    op: str
-    first_un: Unary
-    second_un: Unary
+    left: Expression
+    right: Expression
 
     def tostring(self, n):
-        output = ""
-        output += " " * n + self.op + "\n"
-        output += self.first_un.tostring(n + 2) + "\n"
-        output += self.second_un.tostring(n + 2) + "\n"
+        output = " " * n + "Factor:\n" + self.left.tostring(n + 2)
+        if self.right:
+            output += " " + self.right.tostring(n + 4)
         return output
 
+# Número entero simple
+print("9876")
+valor = Primary(Token(1, 9876, "NUMBER"))
+print(valor.tostring(0))
 
-# Test Primary, Number, String
-print("# Primary Test:")
-primary_token = Token(1, "I am a string", "STRING")
-primary = Primary(primary_token)
-print(primary.tostring(0))
 
-print("# Number Test:")
-number_token = Token(2, "42", "NUMBER")
-number = Number(number_token)
-print(number.tostring(2))
+# Número decimal
+print("98.76")
+valor = Primary(Token(1, 98.76, "NUMBER"))
+print(valor.tostring(0))
 
-print("# String Test:")
-string_token = Token(3, "hello", "STRING")
-string_node = String(string_token)
-print(string_node.tostring(4))
 
-# Test VarDecl containing a Number
-print("# VarDecl Test:")
-var_decl = VarDecl(name="x", expr=Number(Token(4, "100", "NUMBER")))
-print(var_decl.tostring(0))
+# Cadena básica
+print("Hello there!")
+texto = Primary(Token(1, "Hello there!", "STRING"))
+print(texto.tostring(0))
 
-# Test PrintStmt
-print("# PrintStmt Test:")
-print_stmt = PrintStmt(expr=String(Token(5, "A message", "STRING")))
-print(print_stmt.tostring(1))
 
-# Test Block with multiple statements
-declarations = [
-    var_decl,
-    FunDecl(fun=Function(name="foo", parameters=[], body=Block([])))
-]
-block = Block(declarations=declarations)
-print("# Block Test:")
-print(block.tostring(0))
+# Cadena vacía
+print("Cadena vacía")
+texto = Primary(Token(1, "", "STRING"))
+print(texto.tostring(0))
 
-# Test Function
-print("# Function Test:")
-func = Function(
-    name="add",
-    parameters=["a", "b"],
-    body=Block([VarDecl("sum", Number(Token(6, "a + b", "EXPR")))])
-)
-print(func.tostring(0))
 
-# Test IfStmt
-print("# IfStmt Test:")
-if_stmt = IfStmt(
-    condition=Primary(Token(7, "true", "BOOLEAN")),
-    then_branch=Block([PrintStmt(Primary(Token(8, "then branch", "STRING")))]),
-    else_branch=Block([PrintStmt(Primary(Token(9, "else branch", "STRING")))])
-)
-print(if_stmt.tostring(0))
+# Cadena que parece número
+print("999 como texto")
+texto = Primary(Token(1, "999", "STRING"))
+print(texto.tostring(0))
 
-# Test ForStmt
-print("# ForStmt Test:")
-for_stmt = ForStmt(
-    init=VarDecl("i", Number(Token(10, "0", "NUMBER"))),
-    condition=Comparison(left=Primary(Token(11, "i < 3", "EXPR")), right=None),
-    increment=Assignment(name="i", expr=Primary(Token(12, "i + 1", "EXPR"))),
-    body=Block([PrintStmt(Primary(Token(13, "loop", "STRING")))])
-)
-print(for_stmt.tostring(0))
 
-# Test WhileStmt
-print("# WhileStmt Test:")
-while_stmt = WhileStmt(
-    condition=Primary(Token(14, "cond", "EXPR")),
-    body=Block([ExprStmt(Primary(Token(15, "body expr", "EXPR")))])
-)
-print(while_stmt.tostring(0))
+# Suma
+print("operación suma")
+op = Term(Unary("+", [Primary(Token(1, 10, "NUMBER")), Primary(Token(1, 5, "NUMBER"))]), None)
+print(op.tostring(0))
 
-# Test ReturnStmt
-print("# ReturnStmt Test:")
-return_stmt = ReturnStmt(value=Primary(Token(16, "result", "EXPR")))
-print(return_stmt.tostring(0))
 
-# Test Program
-print("# Program Test:")
-program = Program(declarations=[
-    VarDecl("x", Number(Token(17, "5", "NUMBER"))),
-    FunDecl(fun=func),
-    ExprStmt(Primary(Token(18, "end", "STRING")))
-])
-print(program.tostring())
+# Resta
+print("operación resta")
+op = Term(Unary("-", [Primary(Token(1, 10, "NUMBER")), Primary(Token(1, 5, "NUMBER"))]), None)
+print(op.tostring(0))
+
+
+# Multiplicación
+print("producto")
+op = Factor(Unary("*", [Primary(Token(1, 4, "NUMBER")), Primary(Token(1, 3, "NUMBER"))]), None)
+print(op.tostring(0))
+
+
+# División
+print("división")
+op = Factor(Unary("/", [Primary(Token(1, 8, "NUMBER")), Primary(Token(1, 2, "NUMBER"))]), None)
+print(op.tostring(0))
+
+
+# Negativo
+print("negativo")
+op = Unary("-", [Primary(Token(1, 100, "NUMBER"))])
+print(op.tostring(0))
+
+
+# Comparaciones
+print("menor que")
+cmp = Comparison(Term(Unary("<", [Primary(Token(1, 3, "NUMBER")), Primary(Token(1, 7, "NUMBER"))]), None), None)
+print(cmp.tostring(0))
+
+
+print("menor o igual que")
+cmp = Comparison(Term(Unary("<=", [Primary(Token(1, 3, "NUMBER")), Primary(Token(1, 7, "NUMBER"))]), None), None)
+print(cmp.tostring(0))
+
+
+print("mayor que")
+cmp = Comparison(Term(Unary(">", [Primary(Token(1, 3, "NUMBER")), Primary(Token(1, 7, "NUMBER"))]), None), None)
+print(cmp.tostring(0))
+
+
+print("mayor o igual que")
+cmp = Comparison(Term(Unary(">=", [Primary(Token(1, 3, "NUMBER")), Primary(Token(1, 7, "NUMBER"))]), None), None)
+print(cmp.tostring(0))
+
+
+# Igualdades
+print("¿son iguales?")
+igual = Equality(Comparison(Term(Unary("==", [Primary(Token(1, 42, "NUMBER")), Primary(Token(1, 42, "NUMBER"))]), None), None), None)
+print(igual.tostring(0))
+
+
+print("gato es diferente de tigre")
+igual = Equality(Comparison(Term(Unary("!=", [Primary(Token(1, "cat", "STRING")), Primary(Token(1, "tiger", "STRING"))]), None), None), None)
+print(igual.tostring(0))
+
+
+print("321 == \"pi\"")
+igual = Equality(Comparison(Term(Unary("==", [Primary(Token(1, 321, "NUMBER")), Primary(Token(1, "pi", "STRING"))]), None), None), None)
+print(igual.tostring(0))
+
+
+print("456 == \"456\"")
+igual = Equality(Comparison(Term(Unary("==", [Primary(Token(1, 456, "NUMBER")), Primary(Token(1, "456", "STRING"))]), None), None), None)
+print(igual.tostring(0))
+
+
+# Lógicos
+print("¡no verdadero!")
+neg = Unary("!", [Primary(Token(1, True, "BOOLEAN"))])
+print(neg.tostring(0))
+
+
+print("¡no falso!")
+neg = Unary("!", [Primary(Token(1, False, "BOOLEAN"))])
+print(neg.tostring(0))
+
+
+print("y lógico falso")
+log = Logic_and(Equality(Comparison(Term(Factor(Unary("and", [Primary(Token(1, False, "BOOLEAN")), Primary(Token(1, False, "BOOLEAN"))]), None), None), None), None), None)
+print(log.tostring(0))
+
+
+print("y lógico verdadero")
+log = Logic_and(Equality(Comparison(Term(Factor(Unary("and", [Primary(Token(1, True, "BOOLEAN")), Primary(Token(1, True, "BOOLEAN"))]), None), None), None), None), None)
+print(log.tostring(0))
+
+
+print("o lógico falso")
+log = Logic_or(Logic_and(Equality(Comparison(Term(Factor(Unary("or", [Primary(Token(1, False, "BOOLEAN")), Primary(Token(1, False, "BOOLEAN"))]), None), None), None), None), None), None)
+print(log.tostring(0))
+
+
+print("o lógico verdadero")
+log = Logic_or(Logic_and(Equality(Comparison(Term(Factor(Unary("or", [Primary(Token(1, True, "BOOLEAN")), Primary(Token(1, False, "BOOLEAN"))]), None), None), None), None), None), None)
+print(log.tostring(0))
+
+
+# Operación anidada
+print("cálculo complejo")
+comp = Unary("*", [Unary("+", [Primary(Token(1, 5, "NUMBER")), Primary(Token(1, 3, "NUMBER"))]), Primary(Token(1, 10, "NUMBER"))])
+print(comp.tostring(0))
+
+
+# Media
+print("Calculo media")
+media = Assignment("promedio", Unary("/", [Unary("+", [Primary(Token(1, "inicio", "STRING")), Primary(Token(1, "fin", "STRING"))]), Primary(Token(1, 2, "NUMBER"))]))
+print(media.tostring(0))
+
+
+# Imprimir mensaje
+print("Imprimir saludo")
+saludo = PrintStmt(Primary(Token(1, "¡Hola!", "STRING")))
+print(saludo.tostring(0))
+
+
+# Bloque de impresión
+print("Bloque múltiple")
+stmt1 = PrintStmt(Primary(Token(1, "Primera línea", "STRING")))
+stmt2 = PrintStmt(Primary(Token(1, "Segunda línea", "STRING")))
+bloque = Block([stmt1, stmt2])
+print(bloque.tostring(0))
+
+
+# Condicional simple
+print("Condicional básico")
+stmt_if = IfStmt(Primary(Token(1, "cond", "STRING")), Block([PrintStmt(Primary(Token(1, "Sí", "STRING")))]), Block([PrintStmt(Primary(Token(1, "No", "STRING")))]))
+print(stmt_if.tostring(0))
+
+
+# Condicional múltiple
+print("Condicional múltiple")
+stmt_if = IfStmt(Primary(Token(1, "cond", "STRING")),
+    Block([PrintStmt(Primary(Token(1, "Positivo", "STRING")))]),
+    IfStmt(Primary(Token(1, "otraCond", "STRING")),
+        Block([PrintStmt(Primary(Token(1, "Negativo", "STRING")))]),
+        Block([PrintStmt(Primary(Token(1, "Neutro", "STRING")))])))
+print(stmt_if.tostring(0))
+
+
+# While loop
+print("Bucle while")
+decl = VarDecl("contador", Primary(Token(1, 0, "NUMBER")))
+stmt1 = PrintStmt(Primary(Token(1, "contador", "NUMBER")))
+stmt2 = Assignment("contador", Unary("+", [Primary(Token(1, "contador", "NUMBER")), Primary(Token(1, 1, "NUMBER"))]))
+cuerpo = Block([stmt1, stmt2])
+bucle = WhileStmt(Comparison(Term(Factor(Unary("<", [Primary(Token(1, "contador", "NUMBER")), Primary(Token(1, 5, "NUMBER"))]), None), None), None), cuerpo)
+print(Program([decl, bucle]).tostring(0))
+
+
+# Bucle for
+print("Bucle for")
+decl = VarDecl("i", Primary(Token(1, 1, "NUMBER")))
+actualiza = Assignment("i", Unary("+", [Primary(Token(1, "i", "NUMBER")), Primary(Token(1, 1, "NUMBER"))]))
+bloque = Block([PrintStmt(Primary(Token(1, "i", "NUMBER")))])
+bucle = ForStmt(decl, Comparison(Term(Factor(Unary("<", [Primary(Token(1, "i", "NUMBER")), Primary(Token(1, 6, "NUMBER"))]), None), None), None), actualiza, bloque)
+print(bucle.tostring(0))
+
+
+# Función sin retorno
+print("Función suma")
+cuerpo = Block([PrintStmt(Unary("+", [Primary(Token(1, "x", "NUMBER")), Primary(Token(1, "y", "NUMBER"))]))])
+funcion = Function("suma", ["x", "y"], cuerpo)
+print(FunDecl(funcion).tostring(0))
+
+
+# Función con retorno
+print("Función retorna suma")
+retorno = ReturnStmt(Unary("+", [Primary(Token(1, "x", "NUMBER")), Primary(Token(1, "y", "NUMBER"))]))
+funcion = Function("retornaSuma", ["x", "y"], Block([retorno]))
+print(FunDecl(funcion).tostring(0))
+
+
+# Clase
+print("Clase Vehículo")
+metodo1 = Function("arrancar", [], Block([PrintStmt(Primary(Token(1, "Motor encendido", "STRING")))]))
+metodo2 = Function("conducir", ["destino"], Block([PrintStmt(Unary("+", [Primary(Token(1, "Rumbo a ", "STRING")), Primary(Token(1, "destino", "STRING"))]))]))
+clase = ClassDecl("Vehiculo", None, [metodo1, metodo2])
+print(clase.tostring(0))
+
+
+# Instancia de clase
+print("Instancia de Vehículo")
+inst = VarDecl("miAuto", CallAtribute(Primary(Token(1, "Vehiculo", "CLASS")), "Vehiculo()", None))
+print(Program([inst, PrintStmt(inst)]).tostring(0))
+
+
+# Herencia
+print("Herencia Vehículo -> Coche")
+metodo = Function("tocarBocina", [], Block([PrintStmt(Primary(Token(1, "¡Beep beep!", "STRING")))]))
+heredada = ClassDecl("Coche", clase, [metodo])
+print(heredada.tostring(0))

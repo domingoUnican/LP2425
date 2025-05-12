c7d8295223a18f55dd8fa0643d2f3befeaa5c2bc
cgarciabrumfield <cgarciabrumfield@gmail.com>
2025-05-12 21:37:05 +0200

    Corregido toString()

--- Teoria_y_Ejercicios/Lox/Representacion.py
+++ Teoria_y_Ejercicios/Lox/Representacion.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from Lexer import Token
-from typing import List, Optional, Any
+from typing import List, Optional
 
 @dataclass
 class Declaration:
@@ -10,39 +10,26 @@ class Declaration:
 class Primary:
     pass
 
+
 @dataclass
 class Unary:
     op: str
-    atr: Optional["Unary"]=None  # Esto es para representar call o otro Unary
-    def toString(self, n):
-        output = " " * n + str(self.atr) + "\n"
-        output += " " * (n + 2) + str(self.atr)  # Aquí ponemos el valor un poco más indentado
+    atr: Optional["Unary"] = None  # Esto es para representar call o otro Unary
+    def tostring(self, n):
+        output = " " * n + str(self.op) + "\n"
+        output += " " * (n + 2) + str(self.atr)
         return output
 
-    
 @dataclass
 class Call:
     base: Primary
 
 @dataclass
-class CallMethod(Call):
-    primary: Primary
-    args: List
-    additionalCalls: Call | None
-
-@dataclass
-class CallMethod(Call):
-    primary: Primary
-    identifier: str
-    additionalCalls: Call | None 
-
-
-@dataclass
 class Number(Primary):
     tok: Token
-    def toString(self, n):
-        output = " " * n + str(self.tok.tipo) + "\n"
-        output += " " * (n + 2) + str(self.tok.value)  # Aquí ponemos el valor un poco más indentado
+    def tostring(self, n):
+        output = " " * n + self.tok.tipo + "\n"
+        output += " " * (n + 2) + self.tok.value  # Aquí ponemos el valor un poco más indentado
         return output
 
 @dataclass
@@ -51,19 +38,27 @@ class Factor:
     first_un: Unary
     second_un: Unary
 
-    def toString(self, n):
+    def tostring(self, n):
         output =""
         output += " "*n + self.op + "\n"
-        output += self.first_un.toString(n+2) + "\n"
-        output += self.second_un.toString(n+2) + "\n"
+        output += self.first_un.tostring(n+2) + "\n"
+        output += self.second_un.tostring(n+2) + "\n"
         return output
             
+@dataclass
+class Parameter:
+    name: str
+    body: Optional[str]
 
 @dataclass
 class Function:
     name: str
     params: List['Parameter']
-    body: 'Block'
+    
+
+@dataclass
+class Statement(Declaration):
+    pass
 
 @dataclass
 class ClassDeclaration(Declaration):
@@ -76,15 +71,24 @@ class FunctionDeclaration(Declaration):
     fun: Function
 
 @dataclass
-class VarDeclaration(Declaration):
+class Assignment:
     name: str
-    expr: 'Expression'
+    atr: Optional["Assignment"]
+
 
 @dataclass
-class Statement(Declaration):
-    pass
+class Expression(Primary):
+    assgn: Assignment
+
+@dataclass
+class ExprStmt(Statement):
+    expr: Expression
+
+@dataclass
+class VarDeclaration(Declaration):
+    name: str
+    expr: Expression
 
 @dataclass
 class Program:
     declarations: List[Declaration]
-

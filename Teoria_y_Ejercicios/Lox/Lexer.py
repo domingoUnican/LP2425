c0e4d5877760e3b708b60ed1c44ecff1a7bbcbf4
pmf586 <pmf586@alumnos.unican.es>
2025-05-13 22:39:36 +0200

    Practicas Individuales del git acabadas

--- Teoria_y_Ejercicios/Lox/Lexer.py
+++ Teoria_y_Ejercicios/Lox/Lexer.py
@@ -297,15 +297,15 @@ prueba3 = '"esto es un string" b'
 prueba4 = "or and "
 
 # Probar las distintas pruebas para comprobar que funciona
-""" for i in tokenize(prueba1):
-    print("El token es ", i) """
+#for i in tokenize(prueba1):
+    #print("El token es ", i) 
 
 
 
 # # salida de prueba 1
 
 # """
-# [Token(lineno=1, value='a', tipo=TokenType.TIdentifier),Token(lineno=1, value=' ', tipo=TokenType.TSpace),Token(lineno=1, value='=', tipo=TokenType.TEqual),Token(lineno=1, value=' ', tipo=TokenType.TSpace),Token(lineno=1, value='1', tipo=TokenType.TNumber),Token(lineno=2, value='\n ', tipo=TokenType.TSpace),Token(lineno=2, value='a', tipo=TokenType.TIdentifier)]
+# [Token(lineno=1, value='a', tipo=TokenType.TIdentifier),,Token(lineno=1, value='=', tipo=TokenType.TEqual),Token(lineno=1, value='1', tipo=TokenType.TNumber),Token(lineno=2, value='a', tipo=TokenType.TIdentifier)]
 # """
 
 # # salida de prueba 2
@@ -318,11 +318,11 @@ prueba4 = "or and "
 # # salida de prueba 3
 
 # """
-# [Token(lineno=1, value='"esto es un string"', tipo=TokenType.TString),Token(lineno=1, value=' ', tipo=TokenType.TSpace),Token(lineno=1, value='b', tipo=TokenType.TIdentifier)]
+# [Token(lineno=1, value='"esto es un string"', tipo=TokenType.TString),Token(lineno=1, value='b', tipo=TokenType.TIdentifier)]
 # """
 
 # # salida de prueba 4
 
 # """
-# [Token(lineno=1, value='or', tipo=TokenType.TOr),Token(lineno=1, value=' ', tipo=TokenType.TSpace),Token(lineno=1, value='and', tipo=TokenType.TAnd),Token(lineno=1, value=' ', tipo=TokenType.TSpace)]
+# [Token(lineno=1, value='or', tipo=TokenType.TOr),Token(lineno=1, value='and', tipo=TokenType.TAnd)]
 # """

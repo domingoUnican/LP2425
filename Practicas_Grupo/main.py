2cb77eaeda6031264a1962762dfd0ca8f1adeef3
Gonzalo Peña Manterola <gpm178@alumnos.unican.es>
2025-05-13 16:09:18 +0200

    Entrega Final Final

--- Practicas_Grupo/main.py
+++ Practicas_Grupo/main.py
@@ -14,7 +14,7 @@ from Lexer import *
 from Parser import *
 from Clases import *
 
-PRACTICA = "02" # Practica que hay que evaluar
+PRACTICA = "01" # Practica que hay que evaluar
 DEBUG = True   # Decir si se lanzan mensajes de debug
 NUMLINEAS = 3   # Numero de lineas que se muestran antes y después de la no coincidencia
 sys.path.append(DIRECTORIO)

5ffeb63d295381a44a9dc93a8ff484479caa911e
pebmaxim <pebmaxim.anici@gmail.com>
2025-05-13 11:28:32 +0200

    Practicas Grupo

--- Practicas_Grupo/Lexer.py
+++ Practicas_Grupo/Lexer.py
@@ -6,62 +6,153 @@ import re
 import sys
 
 
+# class Comentario(Lexer):
+#     _nivel_anidado = 0
+#     tokens = {}
+
+#     @_(r'\(\*')
+#     def LINEA(self, t):
+#         print("entra comentario")
+#         self._nivel_anidado += 1
+
+#     @_(r'\*\)')
+#     def VOLVER(self, t):
+#         print("vuelve de comentario")
+#         # if self._nivel_anidado > 0: self._nivel_anidado -= 1
+#         # if self._nivel_anidado == 0: self.begin(CoolLexer) #??? 
+#         self._nivel_anidado -= 1
+#         self.begin(CoolLexer)
+    
+
+#     @_(r'\n')
+#     def LINEBREAK(self, t):
+#         self.lineno += 1
+    
+#     @_(r'.')
+#     def PASAR(self, t):
+#         pass
+
 class Comentario(Lexer):
+    _nivel_anidado = 0
     tokens = {}
+
+    @_(r'\(\*')
+    def LINEA(self, t):
+        self._nivel_anidado += 1
+
+    @_(r'\*\)')
+    def VOLVER(self, t):
+        if self._nivel_anidado > 0: self._nivel_anidado -= 1
+        if self._nivel_anidado == 0: self.begin(CoolLexer)
+    
     @_(r'.')
     def PASAR(self, t):
         pass
+
     @_(r'\n')
-    def LINEA(self, t):
+    def LINEBREAK(self, t):
         self.lineno += 1
-    @_(r'\*\)')
-    def VOLVER(self, t):
-        self.begin(CoolLexer)
-
-
-
 
 class CoolLexer(Lexer):
     tokens = {OBJECTID, INT_CONST, BOOL_CONST, TYPEID,
               ELSE, IF, FI, THEN, NOT, IN, CASE, ESAC, CLASS,
               INHERITS, ISVOID, LET, LOOP, NEW, OF,
               POOL, THEN, WHILE, STR_CONST, LE, DARROW, ASSIGN}
-    ignore = '\t '
-    literals = {'.'}
-    ELSE = r'\b[eE][lL][sS][eE]\b'
-    STR_CONST = r'"[a-zA-Z0-9_/]*"'
     
-    @_(r'\b[a-z][A-Z0-9_a-z]*\b')
-    def OBJECTID(self, t):
+    RESERVED_WORDS = ("IF", "FI", "THEN", "NOT", "IN", 
+                      "CASE","ELSE", "ESAC", "CLASS", "INHERITS", 
+                      "ISVOID","LET", "LOOP", "NEW", "OF", "POOL", 
+                      "THEN", "WHILE")
+    ignore = '\t '
+    literals = ('.','+','-','*','/','<','=','(',')','~', ';', '{', '}', 
+               ':', ',', '@', '~')
+    ELSE = r'\b[eE][lL][sS][eE]\b'
+    # STR_CONST = r'\"[^\"]*\"'
+
+    #TODO: arith.cool esta bien pero no lo detecta
+    #TODO: io.cool ESTA MAL, no deberia escribir nada porque es todo un comentario
+    #TODO: integers2.cool no tiene sentido 0b es int 0 y objectid b y lo mismo con los valores 000001 no puede ser 1 tiene que ser 000001
+    #TODO: new_complex.cool esta mal con los parentesis y los brackets???
+    #TODO: arreglar comentarios
+    #TODO: arreglar TAB \t en strings (es una feature y no un bug?)
+    
+    @_(r'\"[^\"]*\"')
+    def STR_CONST(self, t):
+        t.value = t.value.replace("\t", "\\t")
         return t
+    
+    @_(r'\(\*')
+    def IR_BLOQUE(self, t):
+        #Comentario._nivel_anidado += 1 #no hace falta porque al entrar en comentario tmb entra ahi en (*
+        self.begin(Comentario)
+    
+    @_(r'--.*')
+    def IR_LINEA(self, t):
+        # if Comentario._nivel_anidado != 0: pass # deberia dar igual porque al estar dentro de comentario se come todo lo que no sea entrada a comentario de bloque o salida
+        self.lineno += 1
+
+    @_(r'\*\)')
+    def ERROR_PARENTESIS(self, t):
+        t.type = "ERROR \"Unmatched *)\""
+        return t
+
+    @_(r'[a-z][A-Z0-9_a-z]*\b')
+    def OBJECTID(self, t):
+        if t.value.upper() in self.RESERVED_WORDS:
+            t.type = t.value.upper()
+        if t.value.upper() in ["TRUE", "FALSE"]:
+            t.type = "BOOL_CONST"
+            t.value = t.value.upper() == "TRUE"
+        return t
+    
+    @_(r'\d+')
+    def INT_CONST(self, t):
+        t.type = "INT_CONST"
+        t.value = t.value
+        #t.value = int(t.value)
+        #integers2.cool
+        return t
+
     @_(r'\n')
     def LINEBREAK(self, t):
         self.lineno += 1
     
-    @_(r'\b[wW][hH][iI][lL][eE]\b')
-    def WHILE(self, t):
-        t.value = (t.value) + 'dddd'
-        return t
-    @_(r'.')
-    def ERROR(self, t):
-        print(t)
-        if t.value in self.literals:
-            t.type = t.value
-    
-    @_(r'\w+')
+    @_(r'\b[A-Z][A-Z0-9_a-z]*')
     def TYPEID(self, t):
+        if t.value.upper() not in self.RESERVED_WORDS:
+            t.type = "TYPEID"
+        else:
+            t.type = t.value.upper()
         return t
 
-    def error(self, t):
-        self.index += 1
+    @_(r'<-')
+    def ASSIGN(self, t):
+        t.value = "ASSIGN"
+        return t
     
+    @_(r'<=')
+    def LE(self, t):
+        t.value = "LE"
+        return t
+    
+    @_(r'=>')
+    def DARROW(self, t):
+        t.value = "DARROW"
+        return t
+        
+    @_(r'\n')
+    def LINEBREAK(self, t):
+        self.lineno += 1
+
+    @_(r'.')
+    def ERROR(self, t):
+        if t.value in self.literals:
+            t.type = t.value
+            return t
     
     CARACTERES_CONTROL = [bytes.fromhex(i+hex(j)[-1]).decode('ascii')
                           for i in ['0', '1']
                           for j in range(16)] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
-    @_(r'\(\*')
-    def IR(self, t):
-        self.begin(Comentario)
 
     def error(self, t):
         self.index += 1
@@ -78,7 +169,7 @@ class CoolLexer(Lexer):
             elif token.type == 'TYPEID':
                 result += f"{str(token.value)}"
             elif token.type in self.literals:
-                result = f'#{token.lineno} \'{token.type}\' '
+                result = f'#{token.lineno} \'{token.type}\''
             elif token.type == 'STR_CONST':
                 result += token.value
             elif token.type == 'INT_CONST':
--- Practicas_Grupo/main.py
+++ Practicas_Grupo/main.py
@@ -6,24 +6,24 @@ from termcolor import colored
 init()
 
 
-DIRECTORIO = os.path.expanduser("~/Repositorio/Docencia/Asignaturas/LP/Proyecto/")
+DIRECTORIO = os.path.expanduser("./")
 sys.path.append(DIRECTORIO)
 
 from Lexer import *
-#from Parser import *
+from Parser import *
 from Clases import *
 
-PRACTICA = "01" # Practica que hay que evaluar
+PRACTICA = "02" # Practica que hay que evaluar
 DEBUG = True   # Decir si se lanzan mensajes de debug
 NUMLINEAS = 3   # Numero de lineas que se muestran antes y después de la no coincidencia
 sys.path.append(DIRECTORIO)
-DIR = os.path.join(DIRECTORIO, PRACTICA, 'grading')
+DIR = os.path.join(DIRECTORIO, PRACTICA, 'minimos')
 FICHEROS = os.listdir(DIR)
 TESTS = [fich for fich in FICHEROS
          if os.path.isfile(os.path.join(DIR, fich)) and
          re.search(r"^[a-zA-Z].*\.(cool|test|cl)$",fich)]
 TESTS.sort()
-TESTS = ["escapedunprintables.cool"]
+TESTS = ["arithprecedence.test"]
 
 if True:
     for fich in TESTS:
@@ -47,13 +47,16 @@ if True:
                 if DEBUG:
                     texto = re.sub(r'#\d+\b','',texto)
                     resultado = re.sub(r'#\d+\b','',resultado)
+                    
                     nuestro = [linea for linea in texto.split('\n') if linea]
                     bien = [linea for linea in resultado.split('\n') if linea]
                     linea = 0
+                    
                     while nuestro[linea:linea+NUMLINEAS] == bien[linea:linea+NUMLINEAS]:
                         linea += 1
                     print(colored('\n'.join(nuestro[linea:linea+NUMLINEAS]), 'white', 'on_red'))
                     print(colored('\n'.join(bien[linea:linea+NUMLINEAS]), 'blue', 'on_green'))
+                    
                     f = open(os.path.join(DIR, fich)+'.nuestro', 'w')
                     g = open(os.path.join(DIR, fich)+'.bien', 'w')
                     f.write(texto.strip())
@@ -69,7 +72,7 @@ if True:
             g.close()
             j = parser.parse(lexer.tokenize(entrada))
             try:
-                j.Tipo()
+                #j.Tipo()
                 if j and not parser.errores:
                     resultado = '\n'.join([c for c in j.str(0).split('\n')
                                            if c and '#' not in c])
